d7603f66ff56296f6b3425201a8b85aa928dc12c
Sir_Dave <lucasdavidd101@gmail.com>
2023-07-03 20:38:27 +0100

    made some editions

--- Day1_band_name_generator.py
+++ Day1_band_name_generator.py
@@ -10,6 +10,6 @@ print('Welcome to the band name geberator')
 #this line asks the use to input the name of the city they grew up in
 city = input('Which city did you grow up in?\n')
 #this line prompts the user to input the nae of their pet
-pet = input('WHat is the name of your pet?\n')
+pet = input('What is the name of your pet?\n')
 #this line contatenates the string variable 'city' and 'pet' suggesting it as the name for the usr's band name
-print(f'Your band name could be {city} {pet}')
+print(f'Your band name could be "{city} {pet}"')
